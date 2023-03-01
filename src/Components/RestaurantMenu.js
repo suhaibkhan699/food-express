cda86bbd90c926815a3b769e321730a7a5071d65
suhaibkhan699 <khansuhaib699@gmail.com>
2023-03-01 18:59:44 +0000

    firebase and shimmer

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -25,73 +25,76 @@ const RestaurantMenu = () => {
   }
 
   return !restaurant ? (
-    <Shimmer />
-  ) : (
-    <div className="restaurant-menu">
-      <div className="restaurant-summary">
-        <img
-          className="restaurant-img"
-          src={IMG_CDN_URL + restaurant?.cloudinaryImageId}
-          alt={restaurant?.name}
-        />
-        <div className="restaurant-summary-details">
-          <h1 className="restaurant-title">{restaurant?.name}</h1><br />
-          <p className="restaurant-tags">{restaurant?.cuisines?.join(", ")}</p> <br />
-          <p className="restaurant-tags"><i className="fa-solid fa-location-dot"></i> {restaurant?.area}</p> <br />
-          <div className="restaurant-details">
-            <div className="restaurant-rating">
-              <i className="fa-solid fa-star"></i>
-              <span>{restaurant?.avgRating}</span>
-            </div>
-            <div className="restaurant-rating-slash">|</div>
-            {/* <div>{restaurant?.sla?.slaString}</div> */}
-            <div>{restaurant?.totalRatingsString}</div>
-            <div className="restaurant-rating-slash">|</div>
-            <div>{restaurant?.costForTwoMsg}</div>
-          </div>
-        </div>
-      </div>
-
-      <div className="restaurant-menu-content">
-        <div className="menu-items-container">
-          <div className="menu-title-wrap">
-            {/* <h3 className="menu-title">Recommended</h3> */}
-            <p className="menu-count">
-              {Object.keys(restaurant?.menu?.items).length} ITEMS
-            </p>
-          </div>
-          <div className="menu-items-list">
-            {Object.values(restaurant?.menu?.items).map((item) => (
-              <div className="menu-item" key={item?.id}>
-                <div className="menu-item-details">
-                  <h3 className="item-title">{item?.name}</h3>
-                  <p className="item-cost">
-                    {item?.price > 0
-                      ? new Intl.NumberFormat("en-IN", {
-                        style: "currency",
-                        currency: "INR",
-                      }).format(item?.price / 100)
-                      : " "}
-                  </p>
-                  <p className="item-desc">{item?.description}</p>
-                </div>
-                <div className="menu-img-wrapper">
-                  {item?.cloudinaryImageId && (
-                    <img
-                      className="menu-item-img"
-                      src={ITEM_IMG_CDN_URL + item?.cloudinaryImageId}
-                      alt={item?.name}
-                    />
-                  )}
-                  <button className="add-btn"> ADD +</button>
-                </div>
+    <div style={{ marginTop: '100px' }
+    }>
+      <Shimmer />
+    </div >)
+    : (
+      <div className="restaurant-menu">
+        <div className="restaurant-summary">
+          <img
+            className="restaurant-img"
+            src={IMG_CDN_URL + restaurant?.cloudinaryImageId}
+            alt={restaurant?.name}
+          />
+          <div className="restaurant-summary-details">
+            <h1 className="restaurant-title">{restaurant?.name}</h1><br />
+            <p className="restaurant-tags">{restaurant?.cuisines?.join(", ")}</p> <br />
+            <p className="restaurant-tags"><i className="fa-solid fa-location-dot"></i> {restaurant?.area}</p> <br />
+            <div className="restaurant-details">
+              <div className="restaurant-rating">
+                <i className="fa-solid fa-star"></i>
+                <span>{restaurant?.avgRating}</span>
               </div>
-            ))}
+              <div className="restaurant-rating-slash">|</div>
+              {/* <div>{restaurant?.sla?.slaString}</div> */}
+              <div>{restaurant?.totalRatingsString}</div>
+              <div className="restaurant-rating-slash">|</div>
+              <div>{restaurant?.costForTwoMsg}</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="restaurant-menu-content">
+          <div className="menu-items-container">
+            <div className="menu-title-wrap">
+              {/* <h3 className="menu-title">Recommended</h3> */}
+              <p className="menu-count">
+                {Object.keys(restaurant?.menu?.items).length} ITEMS
+              </p>
+            </div>
+            <div className="menu-items-list">
+              {Object.values(restaurant?.menu?.items).map((item) => (
+                <div className="menu-item" key={item?.id}>
+                  <div className="menu-item-details">
+                    <h3 className="item-title">{item?.name}</h3>
+                    <p className="item-cost">
+                      {item?.price > 0
+                        ? new Intl.NumberFormat("en-IN", {
+                          style: "currency",
+                          currency: "INR",
+                        }).format(item?.price / 100)
+                        : " "}
+                    </p>
+                    <p className="item-desc">{item?.description}</p>
+                  </div>
+                  <div className="menu-img-wrapper">
+                    {item?.cloudinaryImageId && (
+                      <img
+                        className="menu-item-img"
+                        src={ITEM_IMG_CDN_URL + item?.cloudinaryImageId}
+                        alt={item?.name}
+                      />
+                    )}
+                    <button className="add-btn"> ADD +</button>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default RestaurantMenu;
